d0cc778b8c256ca67b4aadc2962f9ec8ad009ca1
Divya, Sai <sdd633@ftr.com>
2021-07-08 14:30:16 +0530

    adding global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const express = require('express');
+const http = require('http');
 async function bootstrap() {
+  // const server = express();
+  const port = (process.env.PORT || '3003');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix("/api/v1");
+  await app.listen(port);  
+  
+  // const apiFactory = new NestFactoryStatic();
+  // const api = await apiFactory.create(AppModule, server); 
+  // const adminFactory = new NestFactoryStatic();
+  // const admin = await adminFactory.create(AdminModule, server);
+  // admin.setGlobalPrefix("/api/v1/admin");
+  // await admin.init();
+  // const port = (process.env.PORT || '3003');
+  // console.log("App is running on port " + port)
+  // http.createServer(server).listen(port);
 }
 bootstrap();
